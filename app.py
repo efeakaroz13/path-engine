e08da87380ab16931730a761644d9b90e9c75d1c
Efe Akaröz <69296379+KentelDevelopment@users.noreply.github.com>
2022-06-24 15:18:37 +0300

    Design updates

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ app  =Flask(__name__)
 class views:
 	@app.route("/")
 	def index():
+		username = request.cookies.get("username")
+		password = request.cookies.get("password")
+		if username != None:
+			theval = auth.sign_in(username,password)
+			if theval["login"] == True:
+				return templates.home()
+			else:
+				pass
+				
+
 		return templates.index()
 
 	@app.route("/login",methods=["POST","GET"])
@@ -15,8 +25,18 @@ class views:
 		if request.method == "POST":
 			username = request.form.get("username")
 			password = request.form.get("password")
+			theval = auth.sign_in(username,password)
+			if theval["login"] == True:
+				response = make_response(redirect("/"))
+				response.set_cookie("username",username)
+				response.set_cookie("password",password)
+			else:
+				response = make_response(templates.login_err())
+
+
+
 			
-			response = make_response(redirect("/"))
+			
 			return response
 		return templates.login()
 app.run(debug=True)
