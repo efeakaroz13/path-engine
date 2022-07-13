a98c4f3027405b8dcf12d97a9a89ff7587255dbc
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-07-14 00:21:32 +0300

    templates update

--- templates.py
+++ templates.py
@@ -2,26 +2,52 @@ def index():
 	return """
 		<html>
 			<head>
-				<title>Path Engine - Login</title>
 				<meta charset="UTF-8">
 				<meta http-equiv="X-UA-Compatible" content="IE=edge">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<link rel="shortcut icon" href="/static/fav.png">
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Path Engine</title>
+				<link rel="shortcut icon" href="/static/fav.png">
+				<link rel="preconnect" href="https://fonts.googleapis.com">
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+				<link href="https://fonts.googleapis.com/css2?family=Koulen&family=Merriweather:wght@300&display=swap" rel="stylesheet">
+				
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/js/all.min.js" integrity="sha512-LW9+kKj/cBGHqnI4ok24dUWNR/e8sUD8RLzak1mNw5Ja2JYCmTXJTF5VpgFSw+VoBfpMvPScCo2DnKTIUjrzYw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/solid.min.css" integrity="sha512-LopA1sokwAW/FNZdP+/5q8MGyb9CojL1LTz8JMyu/8YZ8XaCDn1EOm6L7RWIIOHRM7K4jwnHuOmyLZeeeYxSOA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/v4-shims.min.css" integrity="sha512-fSDKn6bxKjJWWaTbtlYfFf+1sRuP/CC+T+jhy26N/s2FcBI89kn6aShnsDmECKd2I1nRlXOButa2A2wzQ8mNEw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+				<!-- Google Fonts -->
+				<link
+				  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+				  rel="stylesheet"
+				/>
+				<!-- MDB -->
+				<link
+				  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.css"
+				  rel="stylesheet"
+				/>
 			</head>
 			<body>
-			<div class="nav">
-				<h3>Path Engine</h3>
-			</div>
-			<div style="margin-left:10px">
-				<p>Make your 2 option games online for free!</p>
-			</div>
-			<center>
+			
+			<nav class="navbar navbar-light " style="background:#c4c4c4">
+			  <a class="navbar-brand" href="/" style="font-family: 'Koulen', cursive;font-family: 'Merriweather', serif;">
+			    <img src="/static/fav.png" width="30" height="30" class="d-inline-block align-top" alt="" style="margin-left:10px;margin-right:10px;">
+			    Path Engine
+			  </a>
+			</nav>
 
-				<br>
-				<h4 style="font-size:25px;" >Login or Register</h4>
-				<button style="width:25%; font-size:20px;" onclick="window.location.assign('/login')">Login</button><br><br><button style="width:25%;font-size:20px;">Register</button>
+
+			<div class="container">
+			<center><br><br>
+				<p>Welcome to <code>Path Engine</code> by Kentel</p>
+					
+				
+				
+				
 			</center>
+			</div>
+			
 			</body>
 
 		</html>
